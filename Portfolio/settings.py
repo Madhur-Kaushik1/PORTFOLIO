e5bb47652b5d8f1de2679bb2034e505cb8e13caa
Madhur Kaushik <madhurkaushik109@gmail.com>
2025-11-10 13:36:39 +0530

    Update Setting.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -48,20 +48,24 @@ MY_APPS = [
     'Projects'
 ]
 
+if os.environ.get('DEBUG', 'True') == 'True':
+    MY_APPS.append('django_browser_reload')
+
 INSTALLED_APPS += MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if DEBUG:
-    INSTALLED_APPS.append('django_browser_reload')
+if os.environ.get('DEBUG', 'True') == 'True':
+    MIDDLEWARE.append('django_browser_reload.middleware.BrowserReloadMiddleware')
 
 ROOT_URLCONF = 'Portfolio.urls'
 
@@ -133,6 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
